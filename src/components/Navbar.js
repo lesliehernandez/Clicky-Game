5a7b5676c106bd953bf8428c70c52388bb3e5577
Leslie Hernandez <lesliehernandez@Leslies-MacBook-Pro.local>
2018-06-30 22:15:38 -0400

    minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default class Navbar extends Component {
       <div>
         <nav>
           <div className="row">
-            <div className="nav-item col-4"> <p className="headerimage"><img src="./img/headerimage.png"></img></p></div>
+            <div className="headerimage"><img src="./img/headerimage.png" width="400px" heigh></img></div>
             <div className="score"> Your score : {this.props.score} | High score : {this.props.topScore} </div>
             <div className="instructions">Click on an image to earn points, but don't click on any more than once!</div>
 
